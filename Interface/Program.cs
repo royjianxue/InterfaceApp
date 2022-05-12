2151271115e2a71be4d5fa873bb3daf75b9d22f5
Jian Xue <royjianxue@yahoo.com>
2022-05-12 19:08:47 -0400

    Add project files.

---
+++ Interface/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your gasoline amount: ");
+
+            int gasoline = int.Parse(Console.ReadLine());
+
+            Car car = new Car(gasoline);
+            car.Drive();
+
+            Console.ReadLine();
+
+        }
+    }
+    public interface IVehiculo
+    {
+        void Drive();
+        bool Refuel(int gasolineToRefuel);
+    }
+    public class Car : IVehiculo
+    {
+        public Car(int gasoline)
+        {
+            Gasoline = gasoline;
+        }
+
+        public int Gasoline { get; set; }
+
+        public void Drive()
+        {
+            if (Gasoline > 0)
+            {
+                Console.WriteLine("Car is Driving.");
+            }
+            else
+            {
+                Console.WriteLine("No Gasoline.");
+            }
+        }
+
+        public bool Refuel(int gasolineToRefuel)
+        {
+            Gasoline += gasolineToRefuel;
+            return true;
+        }
+
+
+    }
+
+
+
+
+}
